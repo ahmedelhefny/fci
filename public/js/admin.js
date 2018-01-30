75beb7bd1527a44fe17d894a55ab0ff8f1fc3f5d
developer <redaawad622@gmail.com>
2018-01-30 04:38:12 +0200

    r13 edit

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,62 +5,487 @@ $(document).ready(function(){
     $("html").niceScroll();
     
 
-    
-var two1=$("#two1");
+
+
+
 var two2=$("#two2");
 var two3=$("#two3");
 var two4=$("#two4");
 var two5=$("#two5");
-    
+var two6=$("#two6");
+var two7=$("#two7");
+var two8=$("#two8");
+var two9=$("#two9");
+var two10=$("#two10");
 var two11=$("#two11");
-var two22=$("#two22");
-var two33=$("#two33");
-var two44=$("#two44");
-var two55=$("#two55");
- 
-two22.click(function(){
-        two22.addClass('active');
-    two33.removeClass('active');
-    two44.removeClass('active');
-    two55.removeClass('active');
-    two2.css('display','block');
-    two3.css('display','none');
-     two4.css('display','none');
-     two5.css('display','none');
+var two12=$("#two12");
+var two13=$("#two13");
+
+
+
+
+
+
+
+
+
+ //1st form   
+
+$("#li2").click(function(){
+    //add class active
+        $("#li2").addClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.show();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
+});
+
+//2st form   
+
+$("#li3").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").addClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.hide();
+    two3.show();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
 });
     
-    two33.click(function(){
-    two33.addClass('active');
-    two22.removeClass('active');
-    two44.removeClass('active');
-    two55.removeClass('active');
-    two3.css('display','block');;
-    two2.css('display','none');
-     two4.css('display','none');
-     two5.css('display','none');
+//3st form   
+
+$("#li4").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").addClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.show();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
 });
     
-        two44.click(function(){
-        two44.addClass('active');
-    two22.removeClass('active');
-    two33.removeClass('active');
-    two55.removeClass('active');
-    two4.css('display','block');
-    two2.css('display','none');
-     two3.css('display','none');
-     two5.css('display','none');
+//4st form   
+
+$("#li5").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").addClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.show();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
 });
     
-        two55.click(function(){
-        two55.addClass('active');
-    two22.removeClass('active');
-    two44.removeClass('active');
-    two33.removeClass('active');
-    two5.css('display','block');
-    two2.css('display','none');
-     two4.css('display','none');
-     two3.css('display','none');
+//5st form   
+
+$("#li6").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").addClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.show();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
 });
+    
+//6st form   
+
+$("#li7").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").addClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.show();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
+});
+    
+//7st form   
+
+$("#li8").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").addClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.show();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
+});
+    
+//8st form   
+
+$("#li9").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").addClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.show();
+    two10.hide();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
+});
+    
+//9st form   
+
+$("#li10").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").addClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.show();
+    two11.hide();
+
+    two12.hide();
+    two13.hide();
+
+   
+});
+    
+//10st form   
+
+$("#li11").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").addClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").removeClass('active');
+
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.show();
+
+    two12.hide();
+    two13.hide();
+
+   
+});
+    
+//11st form   
+
+$("#li12").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").addClass('active');
+    $("#li13").removeClass('active');
+
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+
+    two12.show();
+    two13.hide();
+
+
+   
+});
+
+//12st form   
+
+$("#li13").click(function(){
+    //add class active
+        $("#li2").removeClass('active');
+        $("#li3").removeClass('active');
+
+    $("#li4").removeClass('active');
+    $("#li5").removeClass('active');
+    $("#li6").removeClass('active');
+    $("#li7").removeClass('active');
+    $("#li8").removeClass('active');
+    $("#li9").removeClass('active');
+    $("#li10").removeClass('active');
+    $("#li11").removeClass('active');
+    $("#li12").removeClass('active');
+    $("#li13").addClass('active');
+
+
+    //show form
+
+    two2.hide();
+    two3.hide();
+    two4.hide();
+    two5.hide();
+    two6.hide();
+    two7.hide();
+    two8.hide();
+    two9.hide();
+    two10.hide();
+    two11.hide();
+    two12.hide();
+
+
+    two13.show();
+
+   
+});
+    
+
+    
+   
+    
+
+    
+   
     
     var small=$("#small");
     var large=$("#large");
@@ -74,8 +499,10 @@ two22.click(function(){
         large.css('display','inline');
         h4.css('display','block');
         img.css('margin-top','40px');
+        img.css('margin-right','10px');
+
         small.css('display','none');
-        one.css('width','70%');
+        one.css('width','100px');
         divs.css('margin-left','-120px');
         divs.css('width','109.9%');
 });
@@ -85,7 +512,7 @@ two22.click(function(){
         h4.css('display','inline-block');
         img.css('margin-top','0');
         large.css('display','none');
-        one.css('width','100%');
+        one.css('width','250px');
         divs.css('margin-left','-30px');
         divs.css('width','103%');
 });
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="css/hover-min.css">
     <link rel="stylesheet" href="css/demo-page.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-arabic.css">
+
     <link rel="stylesheet" href="css/admin.css">
     <!--[if lt IE 9]>
             <script src="js/html5shiv.min.js"></script>
@@ -53,50 +55,201 @@
 <!-- End Navbar -->
     
 <section class="bod">
-    <div class="container-fluid">
-        <div class="row">
             <div class="all">
-                <div class="col-lg-2">
+                
                     <div class="one">
-                        <img src="images/pho1.jpg" alt="">
-                        <h4>Reda Awad <br> <span>The Admin</span></h4>
-                        <hr>
-                        <h5>MAIN</h5>
-                        <ul>
-                             <li id="two11"> <i class="fa fa-home"></i> Home </li>
-                            <li id="two22" class="active"> <i class="fa fa-picture-o"></i> change carousel image </li>
-                            <li id="two33"> <i class="fa fa-envelope-open"></i> add department </li>
-                            <li id="two44"> <i class="fa fa-users"></i> edit users </li>
-                            <li id="two55"> <i class="fa fa-star"></i> modify text </li>
-                        </ul>
+                            <img src="images/pho1.jpg" alt="">
+                            <h4> admin name <br> <span>The Admin</span></h4>
+                            <hr>
+                            <h5>MAIN</h5>
+                            <ul>
+                                 <li id="li1"> <i class="fa fa-home"></i> الرئيسية </li>
+                                 <h5>الملتقي</h5>
+                                <li id="li2" class="active"> <i class="fa fa-picture-o"></i> اضافة ملتقي جديد </li>
+                                <li id="li3" class=""> <i class="fa fa-envelope-open"></i> اضافة ندوة </li>
+                                <li id="li4"> <i class="fa fa-users"></i> اضافة صور لندوة </li>
+                                <h5>الرئيسية</h5>
+                                <li id="li5"> <i class="fa fa-star"></i> اضافة الصور المهمة  </li>
+                                <li id="li6"> <i class="fa fa-star"></i> اضافة محاور الملتقي </li>
+    
+                                <li id="li7"> <i class="fa fa-star"></i> اضافة شركاء النجاح </li>
+                                <li id="li8"> <i class="fa fa-star"></i> اضافة صورة اللوجو </li>
+                                <h5>عرض</h5>
+                                <li id="li9"> <i class="fa fa-star"></i> عرض المسجلين بالندوة  </li>
+                                <li id="li10"> <i class="fa fa-star"></i> عرض اﻵراء عن الندوة </li>
+    
+                                <li id="li11"> <i class="fa fa-star"></i> عرض الشكاوي </li>
+                                <h5>المستخدمين</h5>
+                                <li id="li12"> <i class="fa fa-star"></i> اضافة مستخدم جديد </li>
+                                <li id="li13"> <i class="fa fa-star"></i> عرض المستخدمين </li>
+    
+    
+    
+    
+                            </ul>
+                        </div>
+                
+                    <div class='right'>
+
+                    
+                    <div class='row'>
+                        <div class='col-lg-offset-3 col-md-offset-2 col-sm-offset-1 col-lg-6  col-md-8 col-sm-10 col-xs-12 col-lg-offset-3 col-md-offset-2 col-sm-offset-1'>
+                                <div class="div two2" id="two2">
+                                        <h3>اضافة ملتقي جديد</h3>
+                                        <form action="/addCarousal" method="post" enctype="multipart/form-data">
+                                            {{csrf_field()}}
+                                    
+                                            <div class="form-group">
+                                                <label for="exampleInputFile">تاريخ الملتقي</label>
+                                                <input type="text" name="date" id="exampleInputFile" placeholder="2017">
+                                            </div>
+                                    
+                                            <button type="submit" class="btn btn-default">اضافة</button>
+                                        </form>
+                                </div>
+                        </div>
+                        <div class='col-lg-offset-3 col-md-offset-2 col-sm-offset-1 col-lg-6  col-md-8 col-sm-10 col-xs-12 col-lg-offset-3 col-md-offset-2 col-sm-offset-1'>
+                                <div class="div two3" id="two3">
+                                        <h3>اضافة ندوة جديد</h3>
+                                        <form action="/addCarousal" method="post" enctype="multipart/form-data">
+                                            {{csrf_field()}}
+                                    
+                                            <div class="form-group">
+                                                <label for="exampleInputFile"> اسم الندوة</label>
+                                                <input type="text"  id="exampleInputFile" placeholder="2017">
+                                            </div>
+                                            <div class="form-group">
+                                                    <label for="exampleInputFile">  صورة الندوة</label>
+                                                    <input type="file" name="" id="exampleInputFile" >
+                                                </div>
+                                                <div class="form-group">
+                                                        <label for="exampleInputFile"> نبذوة عن الندوة</label>
+                                                        <input type="text" name="" id="exampleInputFile" >
+                                                    </div>
+                                                 <div class="form-group">
+                                                            <label for="exampleInputFile">  بداية الندوة</label>
+                                                            <input type="time" name="" id="exampleInputFile" >
+                                                   </div>
+                                                     <div class="form-group">
+                                                            <label for="exampleInputFile">  نهاية الندوة</label>
+                                                             <input type="time" name="" id="exampleInputFile" >
+                                                      </div>
+                                                      <div class="form-group">
+                                                                    <label for="exampleInputFile">  تاريخ الندوة</label>
+                                                                    <input type="date" name="" id="exampleInputFile" >
+                                                       </div>
+                                                       <div class="form-group">
+                                                            <label for="exampleInputFile">  اسم المتحدث</label>
+                                                            <input type="text" name="" id="exampleInputFile" >
+                                                     </div>
+                                                     <div class="form-group">
+                                                            <label for="exampleInputFile">  نبذة عن المتحدث</label>
+                                                            <input type="text" name="" id="exampleInputFile" >
+                                                     </div>
+                                                     <div class="form-group">
+                                                            <label for="exampleInputFile">   اسم الشركة المنتمي اليها </label>
+                                                            <input type="text" name="" id="exampleInputFile" >
+                                                     </div>
+                                               
+                                               
+
+                                        
+                                    
+                                            <button type="submit" class="btn btn-default">اضافة</button>
+                                        </form>
+                                 </div>
+                        </div>
+                        <div class='col-lg-offset-3 col-md-offset-2 col-sm-offset-1 col-lg-6  col-md-8 col-sm-10 col-xs-12 col-lg-offset-3 col-md-offset-2 col-sm-offset-1'>
+                                <div class="div two4" id="two4">
+                                        <h3> اضافة صورة لندوة</h3>
+                                        <form action="/addCarousal" method="post" enctype="multipart/form-data">
+                                            {{csrf_field()}}
+                                    
+                                            <div class="form-group">
+                                                    <label class="custom-file-label" for="customFile">اختار صورة</label>
+
+                                                    <input type="file" class="custom-file-input" id="customFile">
+                                                </div>
+                                            <select class="form-control">
+                                                    <option selected>Open this select menu</option>
+                                                    <option value="1">One</option>
+                                                    <option value="2">Two</option>
+                                                    <option value="3">Three</option>
+                                                  </select>
+                                                 
+                                                  <br>
+                                    
+                                            <button type="submit" class="btn btn-default">اضافة</button>
+                                        </form>
+                                    </div>
+                        </div>
+                        <div class='col-lg-offset-3 col-md-offset-2 col-sm-offset-1 col-lg-6  col-md-8 col-sm-10 col-xs-12 col-lg-offset-3 col-md-offset-2 col-sm-offset-1'>
+                                <div class="div two5" id="two5">
+                                        <h3>اضافة الصور المهمة</h3>
+                                        <form action="/addCarousal" method="post" enctype="multipart/form-data">
+                                            {{csrf_field()}}
+                                    
+                                            <div class="form-group">
+                                                    <label class="custom-file-label" for="customFile">اختيار الصورة</label>
+
+                                                    <input type="file" class="custom-file-input" id="customFile">
+                                                </div>
+                                    
+                                            <button type="submit" class="btn btn-default">اضافة</button>
+                                        </form>
+                                    </div>
+                        </div>
+                        <div class='col-sm-6'>
+                            
+                        <div class="two6" id="two6">
+                          
+                            </div>
+                        </div>
+                        <div class='col-sm-6'>
+                                <div class="two7" id="two7">
+                            
+                                    </div>
+                        </div>
+                        <div class='col-sm-6'>
+                        
+                                <div class="two8" id="two8">
+                            
+                                    </div>
+                        </div>
+                        <div class='col-sm-6'>
+                                <div class="two9" id="two9">
+                          
+                                    </div>
+                        </div>
+                        <div class='col-sm-6'>
+
+                                <div class="two10" id="two10">
+                            
+                                    </div>
+                        
+                        </div>
+                        <div class='col-sm-6'>
+                                <div class="two11" id="two11">
+                           
+                                    </div>
+                        </div>
+                        <div class='col-sm-6'>
+                                <div class="two12" id="two12">
+                            
+                                    </div>
+                        </div>
+                        <div class='col-sm-6'>
+                                <div class="two13" id="two13">
+                            
+                                    </div>
+                        </div>
+                            
                     </div>
+                        
+                    
+                       
                 </div>
-                <div class="col-lg-10 text-center">
-                        <div class="two2" id="two2">
-                            <h2>Add New Picture 1</h2>
-                            <input type="file"/ class="text-center">
-                            <h5>please choose image with good resolution</h5>
-                            <button class="btn btn-primary">Submit</button>
-                        </div>
-                        <div class="two3" id="two3">
-                            <h2>Add New Picture 2</h2>
-                            <input type="file"/ class="text-center">
-                            <h5>please choose image with good resolution</h5>
-                            <button class="btn btn-success">Submit</button>
-                        </div>
-                        <div class="two4" id="two4">
-                            <h2>Add New Picture 3</h2>
-                            <input type="file"/ class="text-center">
-                            <h5>please choose image with good resolution</h5>
-                            <button class="btn btn-danger">Submit</button>
-                        </div>
-                        <div class="two5" id="two5">
-                            <h2>Add New Picture 4</h2>
-                            <input type="file"/ class="text-center">
-                            <h5>please choose image with good resolution</h5>
-                            <button class="btn btn-primary">Submit</button>
-                        </div>
-                </div>
+
             </div>
         </div>
     </div>
